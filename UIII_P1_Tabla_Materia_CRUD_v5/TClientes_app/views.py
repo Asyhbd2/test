564d240272f0959ad6d819c57c2b6009ebcc3b5a
Angel Tadeo De Leon Ceniceros <a.22308051281165@cbtis128.edu.mx>
2024-11-15 11:12:23 -0600

    Add files via upload

---
+++ UIII_P1_Tabla_Materia_CRUD_v5/TClientes_app/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render,redirect
+from .models import TCliente
+# Create your views here.
+
+def inicio_vista(request):
+    losclientes=TCliente.objects.all()
+    return render(request,"gestionarCliente.html",{"misclientes":losclientes})
+
+def registrarCliente(request):
+    clienteid=request.POST["numid"]
+    nombre=request.POST["txtnombre"]
+    correo=request.POST["txtcorreo"]
+    telefono=request.POST["txttelefono"]
+    direccion=request.POST["txtdireccion"]
+    fecharegistro=request.GET["datefecha"]
+
+    guardarCliente=TCliente.objects.create(
+        clienteid=clienteid,nombre=nombre,correo=correo,telefono=telefono,direccion=direccion,fecharegistro=fecharegistro
+    ) # GUARDA EL REGISTRO
+
+    return redirect("/")
+
+def seleccionarCliente(request,clienteid):
+    cliente=TCliente.objects.get(clienteid=clienteid)
+    return render(request,"editarCliente.html",{"misclientes":cliente})
+
+def editarCliente(request):
+    clienteid=request.POST["numid"]
+    nombre=request.POST["txtnombre"]
+    correo=request.POST["txtcorreo"]
+    telefono=request.POST["txttelefono"]
+    direccion=request.POST["txtdireccion"]
+    fecharegistro=request.POST["datefecha"]
+
+    Cliente=TCliente.objects.get(clienteid=clienteid)
+    Cliente.clienteid=clienteid
+    Cliente.nombre=nombre
+    Cliente.correo=correo
+    Cliente.telefono=telefono
+    Cliente.direccion=direccion
+    Cliente.fecharegistro=fecharegistro
+    return redirect("/")
+
+def borrarCliente(request,clienteid):
+    cliente=TCliente.objects.get(clienteid=clienteid)
+    cliente.delete() # BORRA EL REGISTRO
+
+    return redirect("/")
